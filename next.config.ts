ef5ff069abbd161efba90811147bdfe56cc0ed8a
strange-maker <ast358208@gmail.com>
2026-07-22 11:00:23 +0800

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  typescript: {
-    ignoreBuildErrors: true,
-  },
 };
 
 export default nextConfig;
